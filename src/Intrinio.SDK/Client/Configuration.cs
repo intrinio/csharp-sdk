719124624db39c01491dccc2d254cdbf8ddb41a6
Shawn Snyder <ssnyder@intrinio.com>
2024-06-28 20:24:49 -0500

    No release notes for this build

--- src/Intrinio.SDK/Client/Configuration.cs
+++ src/Intrinio.SDK/Client/Configuration.cs
@@ -421,7 +421,7 @@ namespace Intrinio.SDK.Client
             String report = "C# SDK (Intrinio.SDK) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 2.62.1\n";
+            report += "    Version of the API: 2.62.3\n";
             report += "    SDK Package Version: 7.5.4\n";
 
             return report;

d2ab865f228328a30675352a4d9dadbd6f5145e0
ssnyder-intrinio <87659980+ssnyder-intrinio@users.noreply.github.com>
2026-01-21 14:03:46 -0600

    No release notes for this build

--- src/Intrinio.SDK/Client/Configuration.cs
+++ src/Intrinio.SDK/Client/Configuration.cs
@@ -366,7 +366,7 @@ namespace Intrinio.SDK.Client
             String report = "C# SDK (Intrinio.SDK) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 2.122.2\n";
+            report += "    Version of the API: 2.123.0\n";
             report += "    SDK Package Version: 7.23.0\n";
 
             return report;
